af045b7dc08d37c8e87e27a4ebe4afb9d0ab974c
Fabio Miranda <fmiranda@AL-Hou-fmiranda.local>
2015-06-02 16:57:49 -0500

    Source object created

--- alertLogicAPI.py
+++ alertLogicAPI.py
@@ -6,15 +6,15 @@ from collections import namedtuple
 
 class User:
 	"""A class which represent the user from the API"""
-	def __init__(self, userDict):
-		self.id = userDict.get("id","")
-		self.name = userDict.get("name", "")
-		self.email = userDict.get("email", "")
-		self.active = userDict.get("active", "")
-		self.account_id = userDict.get("account_id", "")
+	def __init__(self, user_dict):
+		self.id = user_dict.get("id","")
+		self.name = user_dict.get("name", "")
+		self.email = user_dict.get("email", "")
+		self.active = user_dict.get("active", "")
+		self.account_id = user_dict.get("account_id", "")
 		Record = namedtuple("Record", "at, by")
-		self.created = Record(userDict.get("created").get("at"), userDict.get("created").get("by"))
-		self.modified = Record(userDict.get("modified").get("at"), userDict.get("modified").get("by"))
+		self.created = Record(user_dict.get("created").get("at"), user_dict.get("created").get("by"))
+		self.modified = Record(user_dict.get("modified").get("at"), user_dict.get("modified").get("by"))
 
 	def __str__(self):
 		return ("ID: " + self.id + "\r\n" +
@@ -52,14 +52,14 @@ class Role:
 		self.name = name
 		self.permissions = dictPermissions
 
-	def fromDict(self, roleDict):
-		self.account_id = roleDict.get("account_id")
-		self.name = roleDict.get("name")
-		self.id = roleDict.get("id")
-		self.permissions = roleDict.get("permissions") #Dictionary of permissions
+	def fromDict(self, role_dict):
+		self.account_id = role_dict.get("account_id")
+		self.name = role_dict.get("name")
+		self.id = role_dict.get("id")
+		self.permissions = role_dict.get("permissions") #Dictionary of permissions
 		Record = namedtuple("Record", "at, by")
-		self.created = Record(roleDict.get("created").get("at"), roleDict.get("created").get("by"))
-		self.modified = Record(roleDict.get("modified").get("at"), roleDict.get("modified").get("by"))
+		self.created = Record(role_dict.get("created").get("at"), role_dict.get("created").get("by"))
+		self.modified = Record(role_dict.get("modified").get("at"), role_dict.get("modified").get("by"))
 
 	def __str__(self):
 		return ("ID: " + self.id + "\r\n" +
@@ -71,14 +71,6 @@ class Role:
 
 class Credential:
 	"""A class which represents the credential from the API"""
-	""""credential":{
-			"name":"Ozone",
-			"type":"iam_role",
-			"iam_role":{
-				"arn":"arn:aws:iam::481746159046:role/RestrictedRole",
-				"external_id":"0000-0012"
-			}
-		}"""
 	def __init__(self, arn = "", external_id = "", name = "", type = "", access_key_id = "", secret_access_key = ""):
 		self.id = ""
 		self.name = name
@@ -88,17 +80,17 @@ class Credential:
 		self.created = None
 		self.modified = None
 
-	def fromDict(self, credentialDict):
-		self.id = credentialDict.get("id", "")
-		self.name = credentialDict.get("name", "")
-		self.type = credentialDict.get("type", "")
+	def fromDict(self, credential_dict):
+		self.id = credential_dict.get("id", "")
+		self.name = credential_dict.get("name", "")
+		self.type = credential_dict.get("type", "")
 		if self.type == "iam_role":
-			self.iam_role = IamRole(credentialDict.get("iam_role").get("arn"), credentialDict.get("iam_role").get("external_id"))
+			self.iam_role = IamRole(credential_dict.get("iam_role").get("arn"), credential_dict.get("iam_role").get("external_id"))
 		elif self.type == "aws_key":
-			self.aws_key = AWSKey(credentialDict.get("aws_key").get("access_key_id"), credentialDict.get("aws_key").get("secret_access_key"))
+			self.aws_key = AWSKey(credential_dict.get("aws_key").get("access_key_id"), credential_dict.get("aws_key").get("secret_access_key"))
 		Record = namedtuple("Record", "at, by")
-		self.created = Record(credentialDict.get("created").get("at"), credentialDict.get("created").get("by"))
-		self.modified = Record(credentialDict.get("modified").get("at"), credentialDict.get("modified").get("by"))
+		self.created = Record(credential_dict.get("created").get("at"), credential_dict.get("created").get("by"))
+		self.modified = Record(credential_dict.get("modified").get("at"), credential_dict.get("modified").get("by"))
 
 	def __str__(self):
 		printableString = ("ID: " + self.id + "\r\n" +
@@ -112,21 +104,121 @@ class Credential:
 							"Modified: at: " + str(self.modified.at) + " by: " + str(self.modified.by) + "\r\n")
 		return printableString
 
-class Config:
+class Source:
+	"""Class which represents a source from the API"""
+	"""{
+    "source": {
+        "config": {
+            "collection_method": "api",
+            "collection_type": "aws" | "gce" | "azure",       /* collection_type specifies wich JSON key contains configuration. */
+            "aws": {},
+        "enabled": true,
+        "name": "route105-east-1",
+        "product_type": "outcomes",
+        "tags": [],
+        "type": "environment"
+    }
+}"""
 	def __init__(self):
-		pass
+		self.id = ""
+		self.type = "environment"
+		self.product_type = "outcomes"
+		self.name = ""
+		self.enabled = True
+		self.config = None
+		self.tags = list()
+		self.status = None
+
+	def fromDict(self, source_dict):
+		self.id = source_dict.get("id")
+		self.name = source_dict.get("name")
+		self.enabled = source_dict.get("enabled")
+		self.product_type = source_dict.get("product_type")
+		self.tags.extend(source_dict.get("tags"))
+		self.type = source_dict.get("type")
+		self.host = source_dict.get("host")
+		self.status = Status(user_dict.get("status"))
+		self.config = Config(user_dict.get("config"))
+		Record = namedtuple("Record", "at, by")
+		self.created = Record(source_dict.get("created").get("at"), source_dict.get("created").get("by"))
+		self.modified = Record(source_dict.get("modified").get("at"), source_dict.get("modified").get("by"))
+
+	def __str__(self):
+		return ("Config: " + "\r\n" +
+				"  ID: " + self.id + "\r\n" +
+				"  Name: " + self.name + "\r\n" +
+				"  Enabled: " + str(self.enabled) + "\r\n" +
+				"  Product Type: " + self.product_type + "\r\n" +
+				"  Type: " + self.type + "\r\n" +
+				"  Tags: " + self.tags + "\r\n" +
+				"  Host: " + self.host + "\r\n" +
+				"  Status: " + self.status + "\r\n" +
+				"  Config: " + self.config + "\r\n" +
+				"  Created: at: " + str(self.created.at) + " by: " + str(self.created.by) + "\r\n" +
+				"  Modified: at: " + str(self.modified.at) + " by: " + str(self.modified.by) + "\r\n")
+
+class Config:
+	"""Class which represents a source configuration from the API"""
+	"""
+	"config": {
+            "collection_method": "api",
+            "collection_type": "aws" | "gce" | "azure",       /* collection_type specifies wich JSON key contains configuration. */
+            "aws": {
+                "credential": {
+                    "id": "4F112A6E-F678-1004-9B87-7831C1BE64D2"
+                },
+                /* Note that the following attributes could become a policy in a future */
+                "discover": true,
+                "scan": true,
+                "scope": {
+                    "include": [
+                        {
+                            "type": "vpc",
+                            "key": "/aws/us-east-1/vpc/vpc-1234"
+                        }
+                    ]
+                }
+            }
+        }
+		"""
+	def __init__(self, config_dict):
+		self.collection_method = config_dict.get("collection_method")
+		self.collection_type = config_dict.get("collection_type")
+		setattr(Config, config_dict.get("collection_type"), config_dict.get(config_dict.get("collection_type")))
+
+	def __str__(self):
+		return ("  Collection Method: " + self.collection_method + "\r\n" +
+				"  Collection Type: " + self.collection_type + "\r\n" +
+				"  " + self.collection_type.capitalize() + ": " + self.__dict[self.collection_type] + "\r\n" +
+				"  Updated: " + self.updated + "\r\n")
+
+
+class Status:
+	"Class which represents the status of a source from the API"""
+	def __init__(self, status_dict):
+		self.status = status_dict.get("status")
+		self.details = list()
+		self.details.extend(status_dict.get("details"))
+		self.timestamp = status_dict.get("timestamp")
+		self.updated = status_dict.get("updated")
+
+	def __str__(self):
+		return ("  Status: " + self.status + "\r\n" +
+				"  Details: " + self.details + "\r\n" +
+				"  Timestamp: " + self.timestamp + "\r\n" +
+				"  Updated: " + self.updated + "\r\n")
+
 
 class AlertLogicAPI:
 	""" Class to make request to the Alert Logic API """
 	def __init__(self):
 		#The API base url
-		self._BASE_URL = "https://integration.cloudinsight.alertlogic.com"
-		#self._BASE_URL = "console.product.dev.alertlogic.com"
-		#self._BASE_URL = "https://api.product.dev.alertlogic.com"
+		#self._BASE_URL = "https://integration.cloudinsight.alertlogic.com"
+		self._BASE_URL = "https://api.product.dev.alertlogic.com"
 		self.token = ""
 		self.user = None
 		self.credentials = None
-		self.roles = []
+		self.roles = list()
 
 	@staticmethod
 	def jdefault(o):
@@ -147,20 +239,20 @@ class AlertLogicAPI:
 		"""getting a 403 Error"""
 		create_roles_url = "/aims/v1/" + role.account_id + "/roles"
 		headers = {"X-AIMS-Auth-Token": self.token}
-		roleDict = {"name": role.name, "permissions" : role.permissions}
-		payload = json.dumps(roleDict, default=AlertLogicAPI.jdefault)
+		role_dict = {"name": role.name, "permissions" : role.permissions}
+		payload = json.dumps(role_dict, default=AlertLogicAPI.jdefault)
 		req = requests.post(self._BASE_URL+create_roles_url, headers=headers, data=payload)
 		if req.status_code == requests.codes.created:
 			response = req.json()
 			role = Role()
-			role.fromDict(roleDict)
+			role.fromDict(role_dict)
 			self.roles.append(role)
 		else:
 			print "Error " + str(req.status_code)
 
-	def deleteRoles(self, roleID):
+	def deleteRoles(self, role_id):
 		"""Untested"""
-		delete_role_url = "/aims/v1/" + self.user.account_id + "/roles/" + roleID
+		delete_role_url = "/aims/v1/" + self.user.account_id + "/roles/" + role_id
 		headers = {"X-AIMS-Auth-Token": self.token}
 		req = requests.delete(self._BASE_URL+delete_credentials_url, headers=headers)
 		print req.text
@@ -178,10 +270,10 @@ class AlertLogicAPI:
 		req = requests.get(self._BASE_URL+list_roles_url, headers=headers)
 		if req.status_code == requests.codes.ok:
 			response = req.json()
-			self.roles = []
-			for roleDict in response.get("roles"):
+			self.roles = list()
+			for role_dict in response.get("roles"):
 				role = Role()
-				role.fromDict(roleDict)
+				role.fromDict(role_dict)
 				self.roles.append(role)
 		else:
 			print "Error " + str(req.status_code)
@@ -221,21 +313,21 @@ class AlertLogicAPI:
 			print "Error " + str(req.status_code)
 
 	def listCredentials(self, filters=""):
-		get_credential_url = "/sources/v1/" + self.user.account_id + "/credentials?" + filters
+		list_credentials_url = "/sources/v1/" + self.user.account_id + "/credentials?" + filters
 		headers = {"X-AIMS-Auth-Token": self.token}
-		req = requests.get(self._BASE_URL+get_credential_url, headers=headers)
+		req = requests.get(self._BASE_URL+list_credentials_url, headers=headers)
 		if req.status_code == requests.codes.ok:
 			response = req.json()
-			self.credentials = []
+			self.credentials = list()
 			for credObj in response.get("credentials"):
 				credential = Credential()
 				credential.fromDict(credObj.get("credential"))
-				self.roles.append(credential)
+				self.credentials.append(credential)
 		else:
 			print "Error " + str(req.status_code)
 
-	def getCredential(self, credentialID):
-		get_credential_url = "/sources/v1/" + self.user.account_id + "/credentials/" + credentialID
+	def getCredential(self, credential_id):
+		get_credential_url = "/sources/v1/" + self.user.account_id + "/credentials/" + credential_id
 		headers = {"X-AIMS-Auth-Token": self.token}
 		req = requests.get(self._BASE_URL+get_credential_url, headers=headers)
 		print req.text
@@ -248,8 +340,8 @@ class AlertLogicAPI:
 		else:
 			print "Error "+ str(req.status_code)
 
-	def deleteCredentials(self, credentialID):
-		delete_credential_url = "/sources/v1/" + self.user.account_id + "/credentials/" + credentialID
+	def deleteCredentials(self, credential_id):
+		delete_credential_url = "/sources/v1/" + self.user.account_id + "/credentials/" + credential_id
 		headers = {"X-AIMS-Auth-Token": self.token}
 		req = requests.delete(self._BASE_URL+delete_credentials_url, headers=headers)
 		print req.text
@@ -260,21 +352,44 @@ class AlertLogicAPI:
 		else:
 			print "Error " + str(req.status_code)
 
+	def listSources(self, filters=""):
+		list_sources_url = "/sources/v1/" + self.user.account_id + "/sources?" + filters
+		headers = {"X-AIMS-Auth-Token": self.token}
+		req = requests.get(self._BASE_URL+list_sources_url, headers=headers)
+		if req.status_code == requests.codes.ok:
+			response = req.json()
+			self.sources = list()
+			for credObj in response.get("credentials"):
+				credential = Credential()
+				credential.fromDict(credObj.get("credential"))
+				self.credentials.append(credential)
+		else:
+			print "Error " + str(req.status_code)
 	def createSource(self):
 		pass
 
 def main():
+	#instance of the API object
 	alAPI = AlertLogicAPI()
+
+	print "Log in the system"
 	alAPI.login("admin@ozone.com", "1newP@ssword")
+
+	"""
 	print "listing credentials"
 	alAPI.listCredentials()
-	for credential in alAPI.roles:
+	for credential in alAPI.credentials:
 		print credential
+	"""
+
 	"""
 	print "listing roles"
 	alAPI.listRoles()
 	for role in alAPI.roles:
 		print role
+	"""
+
+	"""
 	print "creating role"
 	permissions = {"*:own:get:*":"allowed", "*:own:list:*":"allowed", "*::get:*":"allowed", "*::list:*":"allowed"}
 	role = Role(alAPI.user.account_id, "MyRole", permissions)
@@ -286,17 +401,25 @@ def main():
 	alAPI.listRoles()
 	for role in alAPI.roles:
 		print role
-	print "TOKEN:"
-	print alAPI.token"""
-	"""print "ACCOUNT ID:"
-	print alAPI.user.account_id"""
 	"""
+
+
+	print "TOKEN:"
+	print alAPI.token
+	print "ACCOUNT ID:"
+	print alAPI.user.account_id
+
+
+	"""
+	print "Validating credential"
 	credential = Credential("arn:aws:iam::481746159046:role/CloudInsightRole", "10000001", "Fabio Test", "iam_role")
 	#credential = Credential("arn:aws:iam::948063967832:role/Barry-Product-Integration", "67013024", "Fabio Test", "iam_role")
 	if alAPI.validateCredentials(credential):
 		alAPI.storeCredentials(credential)
 	else:
-		print "Invalid credential" """
+		print "Invalid credential"
+	"""
+
 if __name__ == "__main__":
 	main()
 
