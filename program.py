332eeb6c041418044dad6e03f413729461cc9798
Fabio Miranda <fmiranda@AL-Hou-fmiranda.local>
2015-06-10 16:10:32 -0500

    removed try except from menu functions and added to the main function to allow workflow control.

--- program.py
+++ program.py
@@ -4,14 +4,14 @@ from alertLogicAPI import *
 
 def options():
 	print "\r\n"
-	print "=== MENU ==="
+	print "======== MENU ========"
 	print "1. List Environments"
 	print "2. List Credentials"
 	print "3. Create Credential"
 	print "4. Create Environment"
 	print "5. Delete Credential"
 	print "6. Delete Environment"
-	print "7. User information"
+	print "7. User Information"
 	print "\r\n"
 	opt = None
 	while not opt:
@@ -25,56 +25,44 @@ def options():
 
 def listSources(alAPI):
 	print "Listing Environments"
-	try:
-		for idx, source in enumerate(alAPI.listSources("source.type=environment")): #Listing the sources with a filter to environments
-			print "Environment UUID " + source.id
-			print source
-	except requests.exceptions.RequestException as e:
-		print e
+	for source in alAPI.listSources("source.type=environment").itervalues(): #Listing the sources with a filter to environments
+		print "Environment UUID: " + source.id
+		print source
+		print "===================================================================="
 
 def listCredentials(alAPI):
 	print "Listing credentials"
-	try:
-		for idx, lcredential in enumerate(alAPI.listCredentials()): #Listing all the credentials of the user
-			print "Credential UUID " + lcredential.id
-			print lcredential
-	except requests.exceptions.RequestException as e:
-		print e
+	for lcredential in alAPI.listCredentials().itervalues(): #Listing all the credentials of the user
+		print "Credential UUID: " + lcredential.id
+		print lcredential
+		print "===================================================================="
 
 def createCredential(alAPI):
 	print "Creating credential"
-	#print "ARN: arn:aws:iam::948063967832:role/Barry-Product-Integration"
 	arn = raw_input("Enter the ARN: ")
-	#print "External ID: 67013024"
 	external_id = raw_input("Enter the external id: ")
 	credential_name = raw_input("Enter a credential name: ")
 	type = "iam_role"
 	dict_cred_data = { "arn" : arn, "external_id" : external_id }
-	try:
-		print alAPI.createCredential(type, credential_name, dict_cred_data) #Creating a new credential
-	except requests.exceptions.RequestException as e:
-		print e
+	print alAPI.createCredential(type, credential_name, dict_cred_data) #Creating a new credential
 
 def createSource(alAPI):
 	print "Creating a source"
 	credential = None
 	while not credential:
 		try:
-			cred_uuid = int(raw_input("Enter the UUID of the credential: "))
+			cred_uuid = raw_input("Enter the UUID of the credential: ")
 			credential = alAPI.credentials[cred_uuid]
-		except IndexError:
-			print "Invalid number"
+		except KeyError:
+			print "Invalid UUID"
 	source_name = raw_input("Environment name: ")
 	collection_type = "aws"
 	scope = {}
 	discover = bool(raw_input("Discover? Yes or empty for not: "))
 	scan = bool(raw_input("Scan? Yes or empty for not: "))
-	try:
-		source = alAPI.createSource(source_name, collection_type, credential, scope, discover, scan) #Creating an environment
-		print source
-		break
-	except requests.exceptions.RequestException as e:
-		print e
+	source = alAPI.createSource(source_name, collection_type, credential, scope, discover, scan) #Creating an environment
+	print source
+
 loginAttempt = 0
 
 def login(alAPI):
@@ -85,15 +73,10 @@ def login(alAPI):
 		exit(1)
 	print "System Login"
 	print "Please Enter your login information"
-	#username = raw_input("Username: ")
-	#password = raw_input("Password: ")
-	username = "admin@ozone.com"
-	password = "1newP@ssword"
-	try:
-		success = alAPI.login(username, password)
-		return success
-	except requests.exceptions.RequestException as e:
-		print e
+	username = raw_input("Username: ")
+	password = raw_input("Password: ")
+	success = alAPI.login(username, password)
+	return success
 
 def main():
 	#instance of the API object
@@ -109,43 +92,63 @@ def main():
 		menu = options()
 		while menu != 0:
 			if menu == 1:
-				listSources(alAPI)
-			elif menu == 2:
-				listCredentials(alAPI)
-			elif menu == 3:
-				createCredential(alAPI)
-			elif menu == 4:
-				if len(alAPI.credentials) == 0:
-					listCredentials(alAPI)
-				createSource(alAPI)
-			elif menu == 5:
-				if len(alAPI.credentials) == 0:
-					listCredentials(alAPI)
-				print "Deleting Credential"
-				while not credential:
-					try:
-						cred_uuid = int(raw_input("Enter the UUID of the credential: "))
-						credential = alAPI.credentials[cred_uuid]
-						alAPI.deleteCredential(credential.id) #Deleting credential by ID
-						del alAPI.credentials[cred_uuid]
-					except requests.exceptions.RequestException as e:
-						print e
-					except IndexError:
-						print "Invalid UUID"
-			elif menu == 6:
-				if len(alAPI.sources) == 0:
+				try:
 					listSources(alAPI)
-				print "Deleting Environment"
-				while not source:
-					try:
-						source_uuid = int(raw_input("Enter the UUID of the environment: "))
-						source = alAPI.sources[source_uuid]
-						alAPI.deleteSource(source.id) #Deleting source by ID
-						del alAPI.credentials[cred_uuid]
-					except requests.exceptions.RequestException as e:
-						print e
-					except IndexError:
-						print "Invalid UUID"
+				except requests.exceptions.RequestException as e:
+					print e
+			elif menu == 2:
+				try:
+					listCredentials(alAPI)
+				except requests.exceptions.RequestException as e:
+					print e
+			elif menu == 3:
+				try:
+					createCredential(alAPI)
+				except requests.exceptions.RequestException as e:
+					print e
+			elif menu == 4:
+				try:
+					if len(alAPI.credentials) == 0:
+						listCredentials(alAPI)
+					createSource(alAPI)
+				except requests.exceptions.RequestException as e:
+					print e
+			elif menu == 5:
+				try:
+					if len(alAPI.credentials) == 0:
+						listCredentials(alAPI)
+					print "Deleting Credential"
+					credential = None
+					while not credential:
+						try:
+							cred_uuid = raw_input("Enter the UUID of the credential: ")
+							credential = alAPI.credentials[cred_uuid]
+							alAPI.deleteCredential(credential.id) #Deleting credential by ID
+							del alAPI.credentials[cred_uuid]
+						except KeyError:
+							print "Invalid UUID"
+						except requests.exceptions.RequestException as e:
+							print e
+				except requests.exceptions.RequestException as e:
+					print e
+			elif menu == 6:
+				try:
+					if len(alAPI.sources) == 0:
+						listSources(alAPI)
+					print "Deleting Environment"
+					source = None
+					while not source:
+						try:
+							source_uuid = raw_input("Enter the UUID of the environment: ")
+							source = alAPI.sources[source_uuid]
+							alAPI.deleteSource(source.id) #Deleting source by ID
+							del alAPI.sources[source_uuid]
+						except requests.exceptions.RequestException as e:
+							print e
+						except KeyError:
+							print "Invalid UUID"
+				except requests.exceptions.RequestException as e:
+					print e
 			elif menu == 7:
 				print "TOKEN:"
 				print alAPI.token #Show temporary access token information
@@ -154,7 +157,6 @@ def main():
 			else:
 				print "Invalid option"
 			menu = options()
-
 	except requests.exceptions.RequestException as e:
 		print e
 
